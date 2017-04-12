1ce3d4f678574659b0a2bd53850d21150974d6a2
Vesa Eloranta <vesa.eloranta@icloud.com>
2017-04-12 14:12:33 +0300

    cities and states

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
     scope :api, defaults: {format: :json} do
       resources :foos
       resources :bars
+      resources :cities
+      resources :states
     end
 
     get '/ui' => 'ui#index'
